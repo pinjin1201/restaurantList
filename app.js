a3fbbc8f9151a305d1628b1c0724c63963e40f2c
pinjin1201 <knowledge84121@gmail.com>
2021-07-12 20:28:31 +0800

    新增show page、search功能

--- app.js
+++ app.js
@@ -5,9 +5,40 @@ const app = express()
 // 設定主機 port
 const port = 3000
 
+// 載入 Express-handlebars
+const exphbs = require('express-handlebars')
+
+// 載入 restaurant.json
+const restaurants = require('./restaurant.json')
+
+// 設定 把樣板引擎交給 Express-handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// 設定靜態檔案位置
+app.use(express.static('public'))
+
 // Handle request and response here
 app.get('/', (req, res) => {
-  res.send('This is my first Express Web App')
+  res.render('index', { restaurants: restaurants.results })
+})
+
+// 設定 show-page 路徑
+app.get('/restaurants/:restaurants_id', (req, res) => {
+  const restaurant = restaurants.results.find(restaurant => restaurant.id.toString() === req.params.restaurants_id)
+  
+  res.render('show', { restaurant })
+})
+
+// 設定 search 路徑
+app.get('/search', (req, res) => {
+  // console.log('req.query', req.query)
+  const keyword = req.query.keyword
+  const restaurant = restaurants.results.filter(restaurant => {
+    return restaurant.name.includes(keyword)
+  })
+
+  res.render('index', { restaurants: restaurant })
 })
 
 // Start and listen the server
